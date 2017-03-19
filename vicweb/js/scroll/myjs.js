7747dd0f21acc929bbc85671dc0ced664b4c1bd7
ruofanzh <zhangruofan@710>
2017-03-19 12:30:36 -0400

    vicweb

---
+++ vicweb/js/scroll/myjs.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+           $('.aniview').AniView();
+       });
+$('.aniview').AniView();
+var options={
+  animateThreshold: 100,
+  scrollPollInterval:15,
+}
+$('.aniview').AniView(options);

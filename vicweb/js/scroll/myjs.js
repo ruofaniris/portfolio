02d4bac35717f8d5a3aa4dee1888af07d04057ef
ruofaniris <ruofan.zhang710@gmail.com>
2018-10-09 21:31:09 -0700

    ux101

--- vicweb/js/scroll/myjs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-           $('.aniview').AniView();
-       });
-$('.aniview').AniView();
-var options={
-  animateThreshold: 100,
-  scrollPollInterval:15,
-}
-$('.aniview').AniView(options);
